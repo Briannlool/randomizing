ce933e63df8dbc52df4d28bf223d7a365794f239
Brian Chan <brian.chan080@gmail.com>
2017-03-23 14:19:34 +0000

    fixed jquery

--- java.js
+++ java.js
@@ -1,10 +1,9 @@
 /* global $ */ 
 
-
-var games= ["League Of Legends", "Overwatch" , "Vain Glory"]; 
+var games= ["League Of Legends", "Overwatch" , "Vain Glory"];
+ 
 $(document).ready(function(){
- 
- 
+    
  
  $("#games").click(function(){
      var x = Math.floor(Math.random()*games.length); 
